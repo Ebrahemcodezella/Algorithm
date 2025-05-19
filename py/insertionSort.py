52f17e22a122bb0bbede29415072e0914eded18a
Ebrahemcodezella <163308155+Ebrahemcodezella@users.noreply.github.com>
2025-05-19 18:37:53 +0300

    Add files via upload

---
+++ py/insertionSort.py
@@ -0,0 +1,24 @@
+# insertion sort
+# O(n^2)
+def insertion_sort(arr):
+    """
+    Perform insertion sort on the given list.
+
+    Args:
+        arr (list): The list of elements to be sorted.
+
+    Returns:
+        list: The sorted list.
+    """
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+
+
